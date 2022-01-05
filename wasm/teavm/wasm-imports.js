aae3e7d64957329e2d1260f369f0add20189def6
iwangxiaodong <i@wangxiaodong.com>
2022-01-05 23:22:16 +0800

    Create wasm-imports.js

---
+++ wasm/teavm/wasm-imports.js
@@ -0,0 +1,47 @@
+
+//  https://github.com/konsoletyper/teavm/blob/master/core/src/main/resources/org/teavm/backend/wasm/wasm-runtime.js
+var wasmImports = {teavmHeapTrace: {init: (p) => {
+        }}, teavm: {
+        logString: (p) => {
+        }, logInt: (p) => {
+        }, logOutOfMemory: (p) => {
+        },
+        currentTimeMillis: (p) => new Date().getTime()}
+};
+
+function returnStringResult(stringPtr, wasmInstance) {
+    let arrayPtr = wasmInstance.exports.teavm_stringData(stringPtr);
+    let length = wasmInstance.exports.teavm_arrayLength(arrayPtr);
+    let cad = wasmInstance.exports.teavm_charArrayData(arrayPtr);
+    let mb = wasmInstance.exports.memory.buffer;
+    let arrayData = new DataView(mb, cad, length * 2);
+    var text = length > 0 ? "" : null;
+    for (let i = 0; i < length; ++i) { //从对象数据区读起
+        let charCode = arrayData.getUint16(i * 2, true);
+        text += String.fromCharCode(charCode);
+    }
+    return text;
+}
+
+function newStringParam(stringValue, wasmInstance) {
+    let javaArg = wasmInstance.exports.teavm_allocateString(stringValue.length);
+    let javaArgAddress = wasmInstance.exports.teavm_objectArrayData(
+            wasmInstance.exports.teavm_stringData(javaArg));
+    let javaArgData = new DataView(wasmInstance.exports.memory.buffer,
+            javaArgAddress, stringValue.length * 2);
+    for (let j = 0; j < stringValue.length; ++j) {
+        javaArgData.setUint16(j * 2, stringValue.charCodeAt(j), true);
+    }
+    return javaArg;
+}
+
+fetch('http://localhost:8080/releases/classes.wasm')
+        .then(r => r.arrayBuffer()).then(r => {
+    var rr = new Response(r, {headers: {'Content-Type': 'application/wasm'}});
+    WebAssembly.instantiateStreaming(rr, wasmImports)
+            .then(obj => {
+                var rs = obj.instance.exports.process(newStringParam("Hi你好", obj.instance));
+                alert(returnStringResult(rs, obj.instance));
+            })
+            .catch((err) => console.log(err));
+});
